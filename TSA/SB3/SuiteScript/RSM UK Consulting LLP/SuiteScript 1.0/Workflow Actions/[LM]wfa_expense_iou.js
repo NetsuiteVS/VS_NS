d343ac236671e3aa6ba804f77c07ec8f684a767f
NetsuiteVS <87364860+NetsuiteVS@users.noreply.github.com>
2021-07-15 21:24:23 +0200

    commit message

---
+++ TSA/SB3/SuiteScript/RSM UK Consulting LLP/SuiteScript 1.0/Workflow Actions/[LM]wfa_expense_iou.js
@@ -0,0 +1,34 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       15 Feb 2018     Leon Munir       RSM UK CONSULTING LLP
+ *
+ */
+
+/**
+ * @returns {Void} Any or no return value
+ */
+function iouAction() {
+	try {
+		var ctx = nlapiGetContext();
+		var selectedIOU = ctx.getSetting("SCRIPT", "custscript_lm_iou_selected");
+		var results = nlapiSearchRecord("customtransaction_tsa_iou2", null,
+				[ new nlobjSearchFilter('custbody32', null, "anyof",
+						nlapiGetRecordId(), null) ], null);
+		for ( var x in results) {
+			if (results[x].id != selectedIOU) {
+				try {
+					nlapiSubmitField("customtransaction_tsa_iou2",
+							results[x].id, "custbody32", "");
+				} catch (exception) {
+					nlapiLogExecution("DEBUG", "Error removing expense report link from IOU", exception);
+				}
+			}
+		}
+		nlapiSubmitField("customtransaction_tsa_iou2", selectedIOU,
+				"custbody32", nlapiGetRecordId());
+	} catch (mainException) {
+		nlapiLogExecution("DEBUG", "Error adding Expense Report link to IOU", mainException);
+	}
+}
