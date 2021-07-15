9bf0923794429b8963c412edc98477996e502b77
NetsuiteVS <87364860+NetsuiteVS@users.noreply.github.com>
2021-07-15 22:08:10 +0200

    commit message

---
+++ TSA/SB3/SuiteScript/2019_01/RSM UK Consulting LLP/SuiteScript 1.0/User Event/[LM]ues_iou_buttons.js
@@ -0,0 +1,47 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       15 Feb 2018     Leon Munir
+ *
+ */
+
+/**
+ * The recordType (internal id) corresponds to the "Applied To" record in your script deployment. 
+ * @appliedtorecord recordType
+ *   
+ * @param {String} type Operation types: create, edit, view, copy, print, email
+ * @param {nlobjForm} form Current form
+ * @param {nlobjRequest} request Request object
+ * @returns {Void}
+ */
+function iouBeforeLoad(type, form, request)
+{
+	var journalentrySearch = null;
+	var partialRecievedDate = null;
+	var advanceID = null;
+	
+	advanceID = nlapiGetRecordId();
+	
+	journalentrySearch = nlapiSearchRecord("journalentry",null,
+			[
+			   ["type","anyof","Journal"], 
+			   "AND", 
+			   ["custbody_tsa_linked_iou","anyof", advanceID]
+			], 
+			[
+			   new nlobjSearchColumn("mainline"), 
+			   new nlobjSearchColumn("trandate").setSort(true)
+			]
+			);
+	
+	nlapiLogExecution('debug' , 'journalentrySearch', journalentrySearch);
+	
+	if(journalentrySearch)
+	{
+		nlapiSubmitField('customtransaction_tsa_iou2', advanceID, 'custbody_tsapartialreturndate', journalentrySearch[0].getValue("trandate"), true); 
+	}
+	
+	
+	form.setScript("customscript_lm_cls_iou_transaction");
+}
