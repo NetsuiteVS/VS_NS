8a06b3455f6def2a92742a8f965caef05c93ba9f
NetsuiteVS <87364860+NetsuiteVS@users.noreply.github.com>
2021-08-05 21:32:19 +0100

    commit message

--- TSA/SB3/SuiteScript/RSM UK Consulting LLP/SuiteScript 2.0/Client/[rsm]_cl_journal.js
+++ TSA/SB3/SuiteScript/RSM UK Consulting LLP/SuiteScript 2.0/Client/[rsm]_cl_journal.js
@@ -150,6 +150,49 @@ function(record, vs_lib, translation, search, log) {
           	return false;
         }
 
+  // ******** Check Offsetting Related party and Unit *********
+      //Call suitelet - Unit lookup
+      var suitletURL = url.resolveScript({ scriptId:'customscript_tsa_unit_rel_party_lookup', deploymentId:'customdeploy_tsa_unit_rel_party_lookup', returnExternalUrl:true, 
+                                          params: { 'custscript_search_type_prm':"unit", 'custscript_id_prm':tsa_rel_party } 
+                                         });
+      var response = https.get({ url: suitletURL });
+      log.debug("Related_Party_lookup_Call", "response: " + JSON.stringify(response));
+      log.debug("Related_Party_lookup_Call", "returned id: " + response.body);
+      var offsetting_unit=parseInt(response.body);
+
+      //Call suitelet - Related Party lookup
+      var suitletURL = url.resolveScript({ scriptId: 'customscript_tsa_unit_rel_party_lookup', deploymentId: 'customdeploy_tsa_unit_rel_party_lookup',	returnExternalUrl: true, 
+                                          params: { 'custscript_search_type_prm': "relparty", 'custscript_id_prm': unit }
+                                         });
+      var response = https.get({ url: suitletURL });
+      log.debug("Related_Party_lookup_Call", "response: " + JSON.stringify(response));
+      log.debug("Related_Party_lookup_Call", "returned id: " + response.body);
+      var offsetting_relparty=parseInt(response.body);
+      
+      //Check RelParty for Unit
+      if(offsetting_relparty && offsetting_unit){
+          var customrecord_cseg_tsa_relatedparSearchObj = search.create({
+            type: "customrecord_cseg_tsa_relatedpar",
+            filters:  [ ["internalid","anyof",offsetting_relparty],"AND",
+                       ["custrecord_cseg_tsa_relatedpar_n101","anyof",offsetting_unit]
+                      ],
+            columns:  [
+              search.createColumn({ name: "internalid", label: "internalid" })
+            ]
+          });
+          var rp_unit_ok=false;
+          customrecord_cseg_tsa_relatedparSearchObj.run().each(function (result) {
+            console.log("related party unit check is ok: "+result.getValue({ name: 'internalid' }));
+            rp_unit_ok=true;
+          });
+          if(!rp_unit_ok){
+              alert(translation.get({ collection: 'custcollection__tsa_collection_01', key: 'MSG_OFFS_RELPARTY_UNIT', locale: translation.Locale.CURRENT })());
+              return false;
+          }
+      }
+      
+// ******** End - Check Offsetting Related party and Unit *********
+
         //Check if Related Party Record is not complete for InterUnit usage
         var customrecord_cseg_tsa_relatedparSearchObj = search.create({
             type: "customrecord_cseg_tsa_relatedpar",
